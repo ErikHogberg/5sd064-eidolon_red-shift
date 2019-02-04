0b73f14ce13b0bbd03f5d94f9528b73e7f91a225
Erik Högberg <erik.hogberg.93@gmail.com>
2019-02-04 02:45:12 +0100

    parallax layer movement

--- Assets/Prefabs/Environment/Parallax Background/parallaxHostScript.cs
+++ Assets/Prefabs/Environment/Parallax Background/parallaxHostScript.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class ParallaxHostScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
+
+
+		/*
 		foreach (Transform tr in transform) {
 			if (tr.tag == "Parallax Layer") {
 				tr.GetComponent<GameObject>();
 				//Debug.Log("fount layer " + tr.name);
 			}
 		}
+		 */
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		//Debug.Log("Host update");
+
+		foreach (ParallaxScript item in GetComponentsInChildren<ParallaxScript>()) {
+			//Debug.Log("found layer " + item.name);
+			item.transform.position = new Vector3(
+				transform.position.x * item.speed,
+				item.transform.position.y,
+				item.transform.position.z
+			);
+		}
+
 	}
 }
---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	public float speedX = 1;
+	public float speedY = 1;
+	public float globalSpeed = 1;
+
+	//private BoxCollider2D boxCollider;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start() {
+		//speed = 1;
+		rb = GetComponent<Rigidbody2D>();
+		//boxCollider = GetComponent<BoxCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+
+		/*
+			rb.AddForce(new Vector2( 
+			horizontal * speedX * globalSpeed * Time.deltaTime,
+			vertical * speedY * globalSpeed * Time.deltaTime
+		));
+		// */
+
+		//*
+		transform.position += new Vector3(
+			horizontal * speedX * globalSpeed * Time.deltaTime,
+			vertical * speedY * globalSpeed * Time.deltaTime,
+			0
+		);
+		// */
+
+
+
+	}
+}

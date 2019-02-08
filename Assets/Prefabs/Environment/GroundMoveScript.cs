0e55a861f1a02bc164465557dbbda0c18288df17
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-02-08 15:52:35 +0100

    camera y movement with better boundaries

--- Assets/Prefabs/Environment/GroundMoveScript.cs
+++ Assets/Prefabs/Environment/GroundMoveScript.cs
@@ -10,18 +10,25 @@ public class GroundMoveScript : MonoBehaviour {
 
 	//public float minX;
 	public float maxX = 5;
-	public float minY= 2;
-	public float max = 2;
+	public float maxY = 2;
+	public float minY = -2;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate() {
 		if (player.transform.position.x > maxX) {
-			transform.localPosition += new Vector3(-Time.deltaTime*speed, 0, 0);
+			transform.localPosition += new Vector3(-Time.deltaTime * speed, 0, 0);
 		}
+
+		if (player.transform.position.y > maxY) {
+			transform.localPosition += new Vector3(0, -Time.deltaTime * speed, 0);
+		} else if (player.transform.position.y < minY) {
+			transform.localPosition += new Vector3(0, Time.deltaTime * speed, 0);
+		}
+
 	}
 }
--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -9,12 +9,12 @@ public class MoveScript : MonoBehaviour {
 	public float globalSpeed = 1;
 
 	//private BoxCollider2D boxCollider;
-	//private Rigidbody2D rb;
+	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start() {
 		//speed = 1;
-		//rb = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody2D>();
 		//boxCollider = GetComponent<BoxCollider2D>();
 	}
 
@@ -23,14 +23,14 @@ public class MoveScript : MonoBehaviour {
 		float vertical = Input.GetAxis("Vertical");
 		float horizontal = Input.GetAxis("Horizontal");
 
-		/*
+		//*
 			rb.AddForce(new Vector2( 
 			horizontal * speedX * globalSpeed * Time.deltaTime,
 			vertical * speedY * globalSpeed * Time.deltaTime
 		));
 		// */
 
-		//*
+		/*
 		transform.position += new Vector3(
 			horizontal * speedX * globalSpeed * Time.deltaTime,
 			vertical * speedY * globalSpeed * Time.deltaTime,

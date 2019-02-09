6abe806b67477b9d857434fbcd14520fd90753d5
Mateusz Romanowski <mati100@live.se>
2019-02-09 02:18:48 +0100

    Revert "Merge branch 'master' into mateusz"
    This reverts commit a335232277801e98c75da50730be472ee688a60c, reversing
    changes made to 4f392aa0fd6ba574eef9296bb082f62263c46262.

--- Assets/Prefabs/Environment/RepeatingTileLayerScript.cs
+++ Assets/Prefabs/Environment/RepeatingTileLayerScript.cs
@@ -7,45 +7,50 @@ public class RepeatingTileLayerScript : MonoBehaviour {
 	public bool flipOnRepeat = false;
 
 	// Use this for initialization
-	void Start() {
+	void Start () {
 		SpriteRenderer tile = GetComponentInChildren<SpriteRenderer>();
 
 		// create copies of tile if it doesn't already have a series of tiles.
 		//if (Layer.GetComponentsInChildren<SpriteRenderer>().Length == 1) {
 
 		float tileWidth = tile.bounds.size.x;
-		float cameraWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect;
-		//*
+
+		// TODO: create enough tiles to fill screen width
+		/*
 		int i = 0;
-		while (tileWidth * i < cameraWidth) {
+		while (tileWidth * i < Camera.current.rect.width) {
+			RepeatingTileScript newTile = Instantiate<RepeatingTileScript>(item);
+			newTile.transform.parent = item.transform;
+			newTile.transform.position += new Vector3(tileWidth*i,0,0);
 			i++;
-			SpriteRenderer newTile = Instantiate<SpriteRenderer>(tile);
-			newTile.transform.parent = transform;
-			newTile.transform.position += new Vector3(tileWidth * i, 0, 0);
-
-			if (flipOnRepeat) {
-				newTile.flipX = true;
-			}
-
 			if (i > 10) {
 				break;
 			}
 		}
 		// */
 
+		//*
+		SpriteRenderer newTile = Instantiate<SpriteRenderer>(tile);
+		//newTile.transform.position.Set(0, 0, newTile.transform.position.z);
+		newTile.transform.parent = transform;
+		newTile.transform.position += new Vector3(tileWidth, 0, 0);
+
+		if (flipOnRepeat) {
+			newTile.flipX = true;
+		}
+		// */
+
+
+		//}
 	}
 
 	// Update is called once per frame
-	void Update() {
+	void Update () {
 		foreach (var tile in GetComponentsInChildren<SpriteRenderer>()) {
-			float tileWidth = tile.bounds.size.x;
-			float cameraHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
-			float cameraWidth = cameraHalfWidth * 2.0f;
 
-			if (tile.transform.position.x - tileWidth / 2 + cameraHalfWidth < -tileWidth) {
-				tile.transform.position += new Vector3(
-					tileWidth * (Mathf.Floor(cameraWidth / tileWidth) + 2),
-					0, 0);
+			float tileWidth = tile.GetComponent<SpriteRenderer>().bounds.size.x;
+			if (tile.transform.position.x < -tileWidth) {
+				tile.transform.position += new Vector3(tileWidth * 2, 0, 0);
 			}
 
 		}

ce25553d8e0fc1218f95d3be42c0adc16b5236d4
Mateusz Romanowski <mati100@live.se>
2019-02-09 02:13:28 +0100

    Revert "Merge branch 'master' into mateusz"
    This reverts commit a335232277801e98c75da50730be472ee688a60c, reversing
    changes made to 4f392aa0fd6ba574eef9296bb082f62263c46262.

--- Assets/Prefabs/Environment/Parallax Background/parallaxHostScript.cs
+++ Assets/Prefabs/Environment/Parallax Background/parallaxHostScript.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class ParallaxHostScript : MonoBehaviour {
 
-	public float speed;
-
-
 	// Use this for initialization
 	void Start() {
 
+
+		/*
+		foreach (Transform tr in transform) {
+			if (tr.tag == "Parallax Layer") {
+				tr.GetComponent<GameObject>();
+				//Debug.Log("fount layer " + tr.name);
+			}
+		}
+		 */
 	}
 
 	// Update is called once per frame
 	void Update() {
+		//Debug.Log("Host update");
 
-		transform.localPosition = new Vector3(
-			transform.position.x * speed,
-			transform.localPosition.y,
-			transform.localPosition.z
-		);
+		foreach (ParallaxScript item in GetComponentsInChildren<ParallaxScript>()) {
+			//Debug.Log("found layer " + item.name);
+			item.transform.position = new Vector3(
+				transform.position.x * item.speed,
+				item.transform.position.y,
+				item.transform.position.z
+			);
+		}
 
 	}
 }

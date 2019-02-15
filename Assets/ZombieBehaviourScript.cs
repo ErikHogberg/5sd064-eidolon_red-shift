ab6b320e9cfaa69c5dfc0b7e7f32de4049c1f1d9
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-02-15 02:50:17 +0100

    zombie aggressive state, moving until it finds an enemy

--- Assets/ZombieBehaviourScript.cs
+++ Assets/ZombieBehaviourScript.cs
@@ -10,8 +10,7 @@ public enum ZombieState {
 }
 
 
-public class ZombieBehaviourScript : MonoBehaviour
-{
+public class ZombieBehaviourScript : MonoBehaviour {
 
 	// IDEA: create container script for assigning player semi-automatically, similar to parallax and repeating tile layers
 	public GameObject Player;
@@ -26,6 +25,19 @@ public class ZombieBehaviourScript : MonoBehaviour
 	public ZombieState State {
 		get { return state; }
 		set {
+
+			switch (state) {
+				case ZombieState.Aggressive:
+					attacking = false;
+					break;
+				case ZombieState.Defensive:
+					break;
+				case ZombieState.Passive:
+					break;
+				default:
+					break;
+			}
+
 			switch (value) {
 				case ZombieState.Aggressive:
 					break;
@@ -41,29 +53,50 @@ public class ZombieBehaviourScript : MonoBehaviour
 
 	private Rigidbody2D rb;
 
+	private bool attacking = false;
+
 
 	// Start is called before the first frame update
-	void Start()
-	{
+	void Start() {
 		state = InitialState;
 		rb = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
-	void FixedUpdate()
-	{
+	void FixedUpdate() {
 
 		// Sets state to player state, skips if state is unchanged in order to avoid calling state accessor too often
-		if ( state != Player.GetComponent<ZombieControlScript>().HordeState) {
-			state = Player.GetComponent<ZombieControlScript>().HordeState;
+		if (State != Player.GetComponent<ZombieControlScript>().HordeState) {
+			State = Player.GetComponent<ZombieControlScript>().HordeState;
 		}
 
-		switch (state) {
-		//switch (Player.GetComponent<ZombieControlScript>().HordeState) {
+		switch (State) {
+			//switch (Player.GetComponent<ZombieControlScript>().HordeState) {
 			case ZombieState.Aggressive:
+				if (!attacking) {
+					FindEnemy();
+				}
 				break;
 			case ZombieState.Defensive:
 				FollowPlayer();
+				break;
+			case ZombieState.Passive:
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D collision) {
+		switch (State) {
+			case ZombieState.Aggressive:
+				if (collision.tag == "Enemy") {
+					// IDEA: choose an enemy, keep attacking until enemy is dead, out of range, or zombie changes state, then it may choose another target
+					attacking = true;
+					rb.velocity = Vector2.zero;
+				}
+				break;
+			case ZombieState.Defensive:
 				break;
 			case ZombieState.Passive:
 				break;
@@ -92,9 +125,14 @@ public class ZombieBehaviourScript : MonoBehaviour
 		} else {
 			// TODO: reposition zombie ahead of player, then move with player
 			//rb.velocity = Vector2.zero;
-			rb.velocity = Player.GetComponent<Rigidbody2D>().velocity; 
+			rb.velocity = Player.GetComponent<Rigidbody2D>().velocity;
 		}
 
+	}
+
+	private void FindEnemy() {
+		rb.velocity = new Vector2(FollowSpeed * Time.deltaTime, 0);
+		//Collider cl;
 	}
 
 }

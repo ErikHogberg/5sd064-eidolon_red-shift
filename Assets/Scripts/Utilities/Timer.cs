39fd1f7200677379c51c5229f67419115f4bc2fe
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-02-15 13:47:57 +0100

    Zombie attacking, script folder restructure

---
+++ Assets/Scripts/Utilities/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Utilities {
+
+
+	class Timer {
+		float time;
+		bool running;
+
+		public Timer() {
+			running = false;
+		}
+
+		public Timer(float time) {
+			Restart(time);
+		}
+
+		public void Restart(float time) {
+			this.time = time;
+			running = true;
+		}
+
+		public bool Update( float dt ) {
+			if (!running) {
+				return false;
+			}
+
+			time -= dt;
+			if (time < 0) {
+				running = false;
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool IsRunning() {
+			return running;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Zombie/ZombieAttackScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Assets.Scripts.Utilities;
+
+public class ZombieAttackScript : MonoBehaviour {
+
+	private bool attacking = false;
+
+	private Timer attackTimer;
+	private Timer colorTimer;
+
+	// Start is called before the first frame update
+	void Start() {
+		attackTimer = new Timer(1);
+		colorTimer = new Timer();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (attacking) {
+			if (attackTimer.Update(Time.deltaTime)) {
+				attackTimer.Restart(1);
+				transform.parent.GetComponent<SpriteRenderer>().color = Color.green;
+				colorTimer.Restart(.2f);
+			} else {
+				//transform.parent.GetComponent<SpriteRenderer>().color = Color.white;
+			}
+		}
+
+		if (colorTimer.Update(Time.deltaTime)) {
+			transform.parent.GetComponent<SpriteRenderer>().color = Color.white;
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D collision) {
+
+		if (collision.tag == "Enemy") {
+			// attacking set in stay instead of enter just in case zombie overlaps with 2 or more enemies
+			attacking = true;
+			//Debug.Log("Zombie in range");
+		}
+
+	}
+
+	private void OnTriggerExit2D(Collider2D collision) {
+
+		if (collision.tag == "Enemy") {
+			attacking = false;
+		}
+
+	}
+}

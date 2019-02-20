90dcd96a83199d8516b3b05dbe0378f1a9bb3129
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-02-20 03:24:56 +0100

    control zombies with mouse dragging

--- Assets/Scripts/CorpseScript.cs
+++ Assets/Scripts/CorpseScript.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CorpseScript : MonoBehaviour
-{
-    public GameObject Zombie;
-    public void SpawnZombie(GameObject player, GameObject ZombieTypeToSpawn)
-    {
-        var zombie = Instantiate(ZombieTypeToSpawn, transform.position, transform.rotation);
+public class CorpseScript : MonoBehaviour {
+	public GameObject Zombie;
+
+	public void SpawnZombie(ZombieControlScript player, GameObject ZombieTypeToSpawn) {
+		var zombie = Instantiate(ZombieTypeToSpawn, transform.position, transform.rotation);
 		zombie.transform.parent = transform.parent;
 		zombie.GetComponent<ZombieBehaviourScript>().Player = player;
-        Destroy(gameObject);
-    }
+		Destroy(gameObject);
+	}
 }

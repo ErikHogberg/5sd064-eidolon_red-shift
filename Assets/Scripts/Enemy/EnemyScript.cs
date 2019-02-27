be77481d5bc06b501a46f435f90fe86fcddba65b
Mateusz Romanowski <mati100@live.se>
2019-02-27 09:41:02 +0100

    Update EnemyScript.cs

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -6,14 +6,21 @@ public class EnemyScript : MonoBehaviour {
 
 	public int health = 100;
 	public GameObject Corpse;
+    public float Speed = 0.01f;
+
+    private float yMax = 3f;
+    private float yMin = -2f;
+    private float yTarget;
+    private bool arrived = false;
 
 	public int ScoreWorth = 10;
 
 	private void Start()
 	{
-		this.enabled = false;
+        this.enabled = false;
 		gameObject.GetComponentInChildren<EnemyWeaponScript>().enabled = false;
-	}
+        yTarget = Random.Range(yMin, yMax);
+    }
 
 	void Update () {
 		if(health < 0 || health == 0)
@@ -22,10 +29,21 @@ public class EnemyScript : MonoBehaviour {
 			corpse.transform.parent = transform.parent;
 			Destroy(gameObject);
 			Assets.Scripts.Globals.Score += ScoreWorth;
-
 		}
-	}
 
+        AIMove();
+    }
+
+    private void AIMove()
+    {
+        Vector3 destination = new Vector3(transform.position.x, yTarget, transform.position.z);
+        if(transform.position == destination)
+        {
+            yTarget = Random.Range(yMin, yMax);
+            destination = new Vector3(transform.position.x, yTarget, transform.position.z);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, destination, Speed);
+    }
 	private void OnBecameVisible()
 	{
 		this.enabled = true;

5b80229b5b854185e8b1fe33be843ccaf03f7742
Mateusz Romanowski <mati100@live.se>
2019-03-04 08:41:16 +0100

    Melee attacks and Movement Tweaks

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -15,16 +15,20 @@ public class EnemyScript : MonoBehaviour {
 	public GameObject Corpse;
     public float Speed = 0.05f;
     public Type EnemyType;
+    public float movementCooldown = 1f;
 
     private float yMax = 2f;
     private float yMin = -2f;
     private float yTarget;
 
-	public int ScoreWorth = 10;
+    private bool lookingRight = false;
+
+    public int ScoreWorth = 10;
 
 	private void Start()
 	{
         this.enabled = false;
+        movementCooldown = 0f;
 		gameObject.GetComponentInChildren<EnemyWeaponScript>().enabled = false;
         yTarget = Random.Range(yMin, yMax);
     }
@@ -38,20 +42,72 @@ public class EnemyScript : MonoBehaviour {
 			Assets.Scripts.Globals.Score += ScoreWorth;
 		}
 
-        AIMove();
+        switch(EnemyType)
+        {
+            case Type.Archer:
+                RangeMove();
+                break;
+            case Type.Knight:
+                MeleeMove();
+                break;
+            case Type.Peasant:
+                MeleeMove();
+                break;
+            default:
+                break;
+        }
+
+        if (GameObject.FindWithTag("Player").transform.position.x < transform.position.x && lookingRight)
+        {
+            lookingRight = !lookingRight;
+            Flip();
+        }
+        else if (GameObject.FindWithTag("Player").transform.position.x > transform.position.x && !lookingRight)
+        {
+            lookingRight = !lookingRight;
+            Flip();
+        }
     }
 
-    private void AIMove()
+    private void Flip()
     {
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void RangeMove()
+    { 
         Vector3 destination = new Vector3(transform.position.x, yTarget, transform.position.z);
         if(transform.position == destination)
         {
             yTarget = Random.Range(yMin, yMax);
             destination = new Vector3(transform.position.x, yTarget, transform.position.z);
+            movementCooldown = 1f;
         }
-        transform.position = Vector3.MoveTowards(transform.position, destination, Speed);
+
+        if(movementCooldown > 0)
+        {
+            movementCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, Speed);
+        }
     }
-	private void OnBecameVisible()
+
+    private void MeleeMove()
+    {
+        if (movementCooldown > 0)
+        {
+            movementCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("Player").transform.position, Speed);
+            GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+        }
+    }
+
+    private void OnBecameVisible()
 	{
 		this.enabled = true;
 		gameObject.GetComponentInChildren<EnemyWeaponScript>().enabled = true;
--- Assets/Scripts/EnemyWeaponScript.cs
+++ Assets/Scripts/EnemyWeaponScript.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnemyWeaponScript : MonoBehaviour
 {
-	public float BulletSpeed = 12f;
+    public float BulletSpeed = 12f;
     public Transform firePoint;
     public GameObject bulletPrefab;
     private GameObject player;
+    private Collider2D playerInRange;
     private float m_Cooldown = 0f;
+    public int Damage = 10;
     public float Cooldown = 0.1f;
 
     private void Start()
@@ -23,21 +25,61 @@ public class EnemyWeaponScript : MonoBehaviour
         }
         if (m_Cooldown == 0f || m_Cooldown < 0f)
         {
-            Shoot();
-            m_Cooldown = Cooldown;
+            switch (GetComponentInParent<EnemyScript>().EnemyType)
+            {
+                case Type.Archer:
+                    Shoot();
+                    m_Cooldown = Cooldown;
+                    break;
+                case Type.Knight:
+                    Melee();
+                    break;
+                case Type.Peasant:
+                    Melee();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
     void Shoot()
     {
-        if(player)
+        if (player)
         {
             Vector2 direction = player.transform.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             firePoint.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-			//bullet.transform.parent = transform.parent; // same parent as enemy that shot it
-			bullet.GetComponent<EnemyBulletScript>().Speed = BulletSpeed;
+            //bullet.transform.parent = transform.parent; // same parent as enemy that shot it
+            bullet.GetComponent<EnemyBulletScript>().Speed = BulletSpeed;
+        }
+    }
+
+    void Melee()
+    {
+        if(playerInRange != null && playerInRange.name == "Player")
+        {
+            playerInRange.GetComponent<PlayerMovementScript>().TakeDamage(Damage);
+            m_Cooldown = Cooldown;
+            GetComponentInParent<SpriteRenderer>().color = new Color32(164, 164, 164, 255);
+            GetComponentInParent<EnemyScript>().movementCooldown = 1f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Player")
+        {
+            playerInRange = collision;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.name == "Player")
+        {
+            playerInRange = null;
         }
     }
 }

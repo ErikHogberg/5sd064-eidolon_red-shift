e64522bb68bbfb6c53805caea5ba674ed55da0af
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-03-07 13:58:04 +0100

    particle system help scripts (untested)

---
+++ Assets/Scripts/Particles/ReparentParticleSystemOnDestroyScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReparentParticleSystemOnDestroyScript : MonoBehaviour {
+	private void OnDestroy() {
+		GetComponentInChildren<ParticleSystem>().transform.parent = transform.parent;
+	}
+}

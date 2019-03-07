4db696fbae7ec31673de1b83fff2c22341d2972b
Mateusz Romanowski <mati100@live.se>
2019-03-07 14:28:26 +0100

    Fixed Infinite Spawn

--- Assets/Scripts/Enemy/EnemyRespawn.cs
+++ Assets/Scripts/Enemy/EnemyRespawn.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class EnemyRespawn : MonoBehaviour
 {
-    public GameObject Archer, Peasant, Knight;
+    public GameObject Enemy1, Enemy2, Enemy3, Enemy4, Enemy5;
+    public int MaxEnemies = 5;
     public int EnemiesToKill = 20;
-    public int EnemiesAlive = 0;
+    public bool InfiniteSpawn = false;
 
+    private int EnemiesAlive = 0;
     private Vector3 stageDimensions;
     private Vector3 respawnPosition;
     private Transform enemies;
@@ -21,24 +23,85 @@ public class EnemyRespawn : MonoBehaviour
     private void Update()
     {
         respawnPosition = new Vector3(stageDimensions.x + Random.Range(4f, 10f), Random.Range(-2f, 2f), 0);
-        Debug.Log(EnemiesAlive);
-        if (EnemiesAlive < 3)
+        if (EnemiesAlive < MaxEnemies && EnemiesToKill > 1)
         {
-            switch (Random.Range(0, 3))
+            switch (Random.Range(0, 4))
             {
                 case 0:
-                    Instantiate(Archer, respawnPosition, transform.rotation, enemies);
-                    EnemiesAlive++;
-                    break;
+                    if(Enemy1)
+                    {
+                        Instantiate(Enemy1, respawnPosition, transform.rotation, enemies);
+                        EnemiesAlive = EnemiesAlive + 1;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 case 1:
-                    Instantiate(Peasant, respawnPosition, transform.rotation, enemies);
-                    EnemiesAlive++;
-                    break;
+                    if (Enemy2)
+                    {
+                        Instantiate(Enemy2, respawnPosition, transform.rotation, enemies);
+                        EnemiesAlive = EnemiesAlive + 1;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 case 2:
-                    Instantiate(Knight, respawnPosition, transform.rotation, enemies);
-                    EnemiesAlive++;
+                    if (Enemy3)
+                    {
+                        Instantiate(Enemy3, respawnPosition, transform.rotation, enemies);
+                        EnemiesAlive = EnemiesAlive + 1;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                case 3:
+                    if (Enemy4)
+                    {
+                        Instantiate(Enemy4, respawnPosition, transform.rotation, enemies);
+                        EnemiesAlive = EnemiesAlive + 1;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                case 4:
+                    if (Enemy5)
+                    {
+                        Instantiate(Enemy5, respawnPosition, transform.rotation, enemies);
+                        EnemiesAlive = EnemiesAlive + 1;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                default:
                     break;
+
             }
+        }
+    }
+
+    public void EnemyKilled(bool offScreen)
+    {
+        if(!offScreen)
+        {
+            if(!InfiniteSpawn)
+            {
+                EnemiesToKill = EnemiesToKill - 1;
+            }
+            EnemiesAlive = EnemiesAlive - 1;
+        }
+        else
+        {
+            EnemiesAlive = EnemiesAlive - 1;
         }
     }
 }
--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -24,6 +24,7 @@ public class EnemyScript : MonoBehaviour {
     private float yMax = 2f;
     private float yMin = -2f;
     private float yTarget;
+    private GameObject camera;
 
     private bool lookingRight = false;
 
@@ -34,23 +35,24 @@ public class EnemyScript : MonoBehaviour {
         this.enabled = false;
         movementCooldown = 0f;
 		gameObject.GetComponentInChildren<EnemyWeaponScript>().enabled = false;
+        camera = GameObject.Find("Main Camera");
         yTarget = Random.Range(yMin, yMax);
         randomScale = Random.Range(scaleMin, scaleMax);
         transform.localScale = new Vector3(randomScale, randomScale, randomScale);
     }
 
 	void Update () {
-		if(health < 0 || health == 0)
+        if (health < 0 || health == 0)
 		{
-			if (lookingRight) {
+            if (lookingRight) {
 				Flip();
 			}
 			var corpse = Instantiate(Corpse, transform.position, transform.rotation);
 			corpse.transform.parent = transform.parent;
 			Destroy(gameObject);
             Assets.Scripts.Globals.Score += ScoreWorth;
-            GameObject.Find("Main Camera").GetComponent<EnemyRespawn>().EnemiesAlive -= 1;
-		}
+            camera.GetComponent<EnemyRespawn>().EnemyKilled(false);
+        }
 
         switch(EnemyType)
         {
@@ -124,26 +126,19 @@ public class EnemyScript : MonoBehaviour {
 
     private void OnBecameVisible()
 	{
-		this.enabled = true;
+        this.enabled = true;
 		gameObject.GetComponentInChildren<EnemyWeaponScript>().enabled = true;
 	}
 
 	private void OnBecameInvisible()
 	{
-        if(!GameObject.Find("Main Camera").active)
-        {
-            return;
-        }
-        if (transform.position.x < GameObject.Find("Main Camera").transform.position.x)
+        if (gameObject.active == true && gameObject.transform.position.x < camera.transform.position.x)
         {
             Destroy(gameObject);
-            GameObject.Find("Main Camera").GetComponent<EnemyRespawn>().EnemiesAlive -= 1;
-        }
-        else
-        {
-            this.enabled = false;
-            gameObject.GetComponentInChildren<EnemyWeaponScript>().enabled = false;
+            camera.GetComponent<EnemyRespawn>().EnemyKilled(true);
         }
 
+        this.enabled = false;
+        gameObject.GetComponentInChildren<EnemyWeaponScript>().enabled = false;
 	}
 }

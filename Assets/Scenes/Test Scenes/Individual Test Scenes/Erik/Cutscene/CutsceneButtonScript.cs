ac1a1d86bfff63c05d7eb7ae2ca190abbf5733a6
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-03-18 13:57:18 +0100

    cutscene skip button

--- Assets/Scenes/Test Scenes/Individual Test Scenes/Erik/Cutscene/CutsceneButtonScript.cs
+++ Assets/Scenes/Test Scenes/Individual Test Scenes/Erik/Cutscene/CutsceneButtonScript.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CutsceneButtonScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-				GetComponent<Button>().onClick.AddListener(GetComponentInParent<CutsceneFrameScript>().SwitchFrame);
+public class CutsceneButtonScript : MonoBehaviour {
+
+	void Start() {
+		GetComponent<Button>().onClick.AddListener(GetComponentInParent<CutsceneFrameScript>().SwitchFrame);
 
 	}
 
-	// Update is called once per frame
-	void Update()
-    {
-        
-    }
 }

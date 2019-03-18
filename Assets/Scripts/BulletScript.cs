f8247b1a3a07d2f90e011eef5d67d6b4e9e500d6
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-03-18 16:46:51 +0100

    keep particles after bullet destroy

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletScript : MonoBehaviour
-{
+public class BulletScript : MonoBehaviour {
 
-    public float Speed = 25f;
-    public int Damage = 20;
-    private Rigidbody2D rb;
+	public float Speed = 25f;
+	public int Damage = 20;
+	private Rigidbody2D rb;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * Speed;
-    }
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = transform.right * Speed;
+	}
 
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.CompareTag("Enemy"))
-        {
-            collider.GetComponent<EnemyScript>().TakeDamage(Damage);
-            Destroy(gameObject);
-        }
-    }
-    void OnBecameInvisible()
-    {
-        Destroy(gameObject);
-    }
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.CompareTag("Enemy")) {
+			collider.GetComponent<EnemyScript>().TakeDamage(Damage);
+			DestroyBullet();
+		}
+	}
+	void OnBecameInvisible() {
+		DestroyBullet();
+	}
+
+	private void DestroyBullet() {
+
+		ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+		particleSystem.Stop();
+
+		Vector3 particleScale = particleSystem.transform.localScale;
+		Vector3 bulletScale = transform.localScale;
+
+		particleScale.x /= bulletScale.x;
+		particleScale.y /= bulletScale.y;
+
+		particleSystem.transform.localScale = particleScale;
+		particleSystem.transform.parent = transform.parent;
+
+		Destroy(gameObject);
+	}
 }
 

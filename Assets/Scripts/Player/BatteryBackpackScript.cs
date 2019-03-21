af5cb20bb0bbddc58b18a66182ddd5445d4bc2fa
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-03-21 15:57:52 +0100

    dynamic battery backpack

--- Assets/Scripts/Player/BatteryBackpackScript.cs
+++ Assets/Scripts/Player/BatteryBackpackScript.cs
@@ -4,64 +4,21 @@ using UnityEngine;
 
 public class BatteryBackpackScript : MonoBehaviour {
 
-	public GameObject Battery1;
-	public GameObject Battery2;
-	public GameObject Battery3;
-	public GameObject Battery4;
-	public GameObject Battery5;
-	public GameObject Battery6;
-	//public List<GameObject> Batteries;
-
-	void Start() {
-
-	}
-
-	void Update() {
-
-	}
-
-	/*
-	private int CountBatteries() {
-		int batteries = 1; // always start with one battery thats not a separate sprite
-		foreach (GameObject child in GetComponentsInChildren<GameObject>()) {
-			if (child.tag == "Battery") {
-				batteries++;
-			}
-		}
-
-		return batteries;
-	}
-	 */
-
-	/*
-	public void AddBattery() {
-		int numberOfBatteries = CountBatteries();
-		if (numberOfBatteries == 1) {
-			GetComponentInChildren<GameObject>().SetActive(true);
-		}
-	}
-	 */
+	public List<GameObject> Batteries;
 
 	public void SetActiveBatteries(int Count) {
-		if (Count > 0 && Battery1 != null) {
-			Battery1.SetActive(true);
-		}
-		if (Count > 1 && Battery2 != null) {
-			Battery2.SetActive(true);
-		}
-		if (Count > 2 && Battery3 != null) {
-			Battery3.SetActive(true);
-		}
-		if (Count > 3 && Battery4 != null) {
-			Battery4.SetActive(true);
-		}
-		if (Count > 4 && Battery5 != null) {
-			Battery5.SetActive(true);
-		}
-		if (Count > 5 && Battery6 != null) {
-			Battery6.SetActive(true);
-		}
 
+		for (int i = 0; i < Count; i++) {
+			if (i >= Batteries.Count) {
+				return;
+			}
+
+			if (Batteries[i] != null) {
+				Batteries[i].SetActive(true);
+			}
+
+		}
+		
 	}
 
 }

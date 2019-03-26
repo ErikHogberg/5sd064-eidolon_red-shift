5765abfd48a3c880efe793d5160a37978c816cd9
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-03-26 16:14:06 +0100

    combined persistant zombie scripts

---
+++ Assets/Scripts/Player/PlayerStartWithZombiesScript.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartWithZombiesScript : MonoBehaviour {
+
+	public int StartZombieOverride = 0;
+	public ZombieType type = ZombieType.Small;
+
+	public List<GameObject> Zombies;
+
+	void Start() {
+		int zombieCount = Globals.StartSmallZombies;
+		switch (type) {
+			case ZombieType.Small:
+				zombieCount = Globals.StartSmallZombies;
+				Globals.StartSmallZombies = 0;
+				break;
+			case ZombieType.Large:
+				zombieCount = Globals.StartLargeZombies;
+				Globals.StartSmallZombies = 0;
+				break;
+		}
+		if (StartZombieOverride > Globals.StartSmallZombies) {
+			zombieCount = StartZombieOverride;
+		}
+
+		for (int i = 0; i < zombieCount; i++) {
+			Zombies[i].SetActive(true);
+			Zombies[i].transform.parent = transform.parent.parent.parent;
+		}
+		
+	}
+
+}

6ef9d8fd4222e2574b7d6c16fdf83a8652f393f4
Nike Törnros <nike.tornros@me.com>
2019-03-27 14:57:23 +0100

    asd

--- Assets/Scripts/Menu/CreditsScroll.cs
+++ Assets/Scripts/Menu/CreditsScroll.cs
@@ -6,11 +6,19 @@ public class CreditsScroll : MonoBehaviour
 {
 
     public float ScrollSpeed;
+    public float SpaceSpeed = 2.0f;
 
     void Update()
     {
+        float speed = ScrollSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speed *= SpaceSpeed;
+        }
+
         Vector3 pos = transform.position;
-        pos.y += ScrollSpeed * Time.deltaTime;
+        pos.y += speed * Time.deltaTime;
         transform.position = pos;
 
     }

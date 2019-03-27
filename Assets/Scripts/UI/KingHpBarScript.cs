5bfc9b01257819bec9123adae04bc57539236c50
Erik Högberg <erik.hogberg.9945@student.uu.se>
2019-03-27 17:59:52 +0100

    boss hp bars

---
+++ Assets/Scripts/UI/KingHpBarScript.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingHpBarScript : MonoBehaviour {
+
+	public GameObject HpBar;
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+		float HpPercentage = Globals.Player.GetHpPercentage();
+		float OverflowPercentage = 0.0f;
+
+		if (HpPercentage > 1.0) {
+			OverflowPercentage = HpPercentage - 1.0f;
+			HpPercentage = 1.0f;
+		} else if (HpPercentage < 0.0f) {
+			HpPercentage = 0.0f;
+		}
+
+		Vector3 hpScale = HpBar.transform.localScale;
+
+		hpScale.x = HpPercentage;
+
+		HpBar.transform.localScale = hpScale;
+
+	}
+
+}
